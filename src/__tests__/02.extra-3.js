d64858d55ad71e6d62c549a770c0837aaba548fc
Ching-Ju Hsieh <chingjshieh@gmail.com>
2021-02-16 23:10:13 +0800

    test: update extra-3 test

--- src/__tests__/02.extra-3.js
+++ src/__tests__/02.extra-3.js
@@ -1,9 +1,9 @@
 import * as React from 'react'
-import {alfredTip} from '@kentcdodds/react-workshop-app/test-utils'
-import {render, screen} from '@testing-library/react'
+import { alfredTip } from '@kentcdodds/react-workshop-app/test-utils'
+import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import App from '../final/02.extra-3'
-// import App from '../exercise/02'
+// import App from '../final/02.extra-3'
+import App from '../exercise/02.extra-3'
 
 beforeEach(() => {
   jest.spyOn(window, 'fetch')
@@ -16,7 +16,7 @@ afterEach(() => {
 })
 
 test('displays the pokemon', async () => {
-  const {unmount} = render(<App />)
+  const { unmount } = render(<App />)
   const input = screen.getByLabelText(/pokemon/i)
   const submit = screen.getByText(/^submit$/i)
 
@@ -24,21 +24,21 @@ test('displays the pokemon', async () => {
   userEvent.type(input, 'pikachu')
   userEvent.click(submit)
 
-  await screen.findByRole('heading', {name: /pikachu/i})
+  await screen.findByRole('heading', { name: /pikachu/i })
 
   // verify that a request is made when props change
   userEvent.clear(input)
   userEvent.type(input, 'ditto')
   userEvent.click(submit)
 
-  await screen.findByRole('heading', {name: /ditto/i})
+  await screen.findByRole('heading', { name: /ditto/i })
 
   // verify that when props remain the same a request is not made
   window.fetch.mockClear()
 
   userEvent.click(submit)
 
-  await screen.findByRole('heading', {name: /ditto/i})
+  await screen.findByRole('heading', { name: /ditto/i })
 
   alfredTip(
     () => expect(window.fetch).not.toHaveBeenCalled(),
@@ -46,7 +46,7 @@ test('displays the pokemon', async () => {
   )
 
   // verify error handling
-  console.error.mockImplementation(() => {})
+  console.error.mockImplementation(() => { })
 
   userEvent.clear(input)
   userEvent.type(input, 'george')
